47e9f4440f11c0c2c3c44376462ad0bec2154312
Blair K <ordinaryjoe@live.com>
2018-01-14 23:22:18 -0500

    derp

--- Module4/js/script.js
+++ Module4/js/script.js
@@ -1,6 +1,28 @@
-function startLogic() {
-    var newHtml = '<em>New Header</em>';
-    document.getElementById('headerItem').innerHTML = newHtml;
-}
+// script for intro.html
+// function startLogic() {
+//     var newHtml = '<em>New Header</em>';
+//     document.getElementById('headerItem').innerHTML = newHtml;
+    
+// }
 
-startLogic();
+// startLogic();
+
+// function clickHandler() {
+//     alert('You clicked me!');
+// }
+
+//script for types.html 
+//var $elem = document.getElementById('output');
+//$elem.innerHTML += 'Test'
+//$elem.innerHTML += 4 + 6; 
+//$elem.innerHTML += 'Demo ' + 4 + 6;
+//$elem.innerHTML += 'Demo ' + (4 + 6);
+
+var testVariable = null;
+var isUndefined = (testVariable === undefined);
+var isNull = (testVariable === null);
+var isEmpty = (testVariable === '');
+
+document.getElementById('isUndefined').innerHTML = isUndefined;
+document.getElementById('isNull').innerHTML = isNull;
+document.getElementById('isEmpty').innerHTML = isEmpty;
